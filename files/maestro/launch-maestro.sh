dd8c922b3a4256b2ef220d2f6ec989aaa360b836
Michael Ray <mjray@umich.edu>
2020-06-30 16:12:35 -0500

    Check network disable field in maestro config

--- files/maestro/launch-maestro.sh
+++ files/maestro/launch-maestro.sh
@@ -7,15 +7,20 @@ fi
 
 config_file=$SNAP_DATA/maestro-config.yaml
 
-# Obtain interfaces maestro is managing
-interfaces=$($SNAP/bin/yq r $config_file network.interfaces.*.if_name)
+networking_disabled=$($SNAP/bin/yq r $config_file network.disable)
 
-# If interfaces found, turn of nmcli management of said interfaces
-[ $? = 0 ] && for i in $interfaces; do
-    $SNAP/bin/nmcli dev set $i managed no
-done
+if [ "$networking_disabled" != "true" ]; then
 
-# @todo: turn on management of previous interfaces that were disabled
-#        and are no longer managed by maestro
+	# Obtain interfaces maestro is managing
+	interfaces=$($SNAP/bin/yq r $config_file network.interfaces.*.if_name)
 
+	# If interfaces found, turn of nmcli management of said interfaces
+	[ $? = 0 ] && for i in $interfaces; do
+	    $SNAP/bin/nmcli dev set $i managed no
+	done
+
+	# @todo: turn on management of previous interfaces that were disabled
+	#        and are no longer managed by maestro
+
+fi 
 exec ${SNAP}/wigwag/system/bin/maestro -config $config_file
